d929f59467a27de9decb7ca5b73f981899a80ca2
RAMOHAM <reda.ali.moham@gmail.com>
2026-03-31 22:01:01 +0100

    initial commit

---
+++ src/main/java/org/example/gamesroomserver/ArenaServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.gamesroomserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesRoomServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gamesroomserver/ArenaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gamesroomserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
